e4fa3add8db039edadcec7ce643aaeabc40757b4
kuzxnia <kacper.kuzniarski@gmail.com>
2020-04-04 23:11:30 +0200

    finished basic typing checker

--- typer.py
+++ typer.py
@@ -1,5 +1,6 @@
 import curses
 import logging
+from time import time
 logging.basicConfig(
     filename='typer.log',
     filemode='a',
@@ -60,6 +61,7 @@ def run(stdscr):
     change = 0
     current_word = 0
     current_row = 0
+    correct_words_len = 0
     # Clear and refresh the screen for a blank canvas
     stdscr.clear()
     stdscr.refresh()
@@ -71,8 +73,10 @@ def run(stdscr):
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, 239)  # white on dark gray
     curses.init_pair(4, 48, curses.COLOR_BLACK)  # green on default
+    curses.init_pair(5, curses.COLOR_BLACK, curses.COLOR_WHITE)
     writen_text = ''
 
+    start_time = 0
     while (key != ord('q')):
         stdscr.clear()
         height, width = stdscr.getmaxyx()
@@ -85,38 +89,50 @@ def run(stdscr):
                 writen_text = writen_text[:-1]
         elif key == ord(' ') or key == 10:
             change = 0
-            if len(rows[current_row]) == current_word:
+
+            current_word += 1
+            if len(rows[current_row]) == current_word - 1:
                 current_word = 1
                 current_row += 1
-            else:
-                current_word += 1
+                log.info(f'next row {current_row}')
+
             answers[current_row].append(writen_text)
             writen_text = ''
-            # accepting word
 
         elif key != 0:
             writen_text += chr(key)
             change += 1
 
+        if current_row == len(rows) - 1 and len(rows[current_row]) == len(answers[current_row]):
+            execution_time = time() - start_time
+            log.info(f'cpm = {correct_words_len} time = {execution_time} {execution_time / 60.0}')
+            cpm = correct_words_len // (execution_time / 60.0)
+            statusbarstr = f"Press 'q' to exit | CPM = {cpm} WPM = {cpm/5}"
+            stdscr.attron(curses.color_pair(5))
+            stdscr.addstr(height - 1, 0, statusbarstr)
+            stdscr.addstr(height - 1, len(statusbarstr), " " * (width - len(statusbarstr) - 1))
+            stdscr.attroff(curses.color_pair(5))
+            stdscr.refresh()
+            log.info('render submit')
+            while (key != ord('q')):
+                key = stdscr.getch()
+            exit()
+
+        correct_words_len = 0
         # Turning on attributes for title
         stdscr.attron(curses.A_BOLD)
-
         # Rendering words
         temp_x = start_x
         color_set = False
         for row_index, row in enumerate(rows):
-            # default
-            # red
-            # green
             for word_index, word in enumerate(row):
                 if current_row == row_index and current_word > word_index or current_row > row_index:
                     if word == answers[row_index][word_index]:
                         stdscr.attron(curses.color_pair(4))
+                        correct_words_len += len(word)
                     else:
                         stdscr.attron(curses.color_pair(2))
                     color_set = True
-                else:
-                    color_set = False
 
                 stdscr.addstr(start_y + row_index, temp_x, word)
                 temp_x += len(word) + 1
@@ -124,9 +140,9 @@ def run(stdscr):
                 if color_set:
                     stdscr.attroff(curses.color_pair(4))
                     stdscr.attroff(curses.color_pair(2))
+                color_set = False
 
             temp_x = start_x
-        log.info('after render ')
         # Turning off attributes for title
         stdscr.attroff(curses.A_BOLD)
 
@@ -141,6 +157,9 @@ def run(stdscr):
 
         # Wait for next input
         key = stdscr.getch()
+
+        if not start_time:
+            start_time = time()
 
 
 if __name__ == '__main__':
